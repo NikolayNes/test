689bd18c7d5725e65fd8bb92ee15d4f3bc88a618
Nikolay Nesvitiy <nikolaynesvitiy@gmail.com>
2017-08-09 19:52:35 +0300

    feat(test):new test for shoes

---
+++ e2e-shoes/pages/main.po.ts
@@ -0,0 +1,20 @@
+import {$, $$, browser, by, element, ElementArrayFinder, ElementFinder} from "protractor";
+
+export class MainPage {
+    linkVittoRossi: ElementFinder = $('a[href*="brand-vitto"]');
+
+    advancedSearchForShoes: ElementFinder = $('a[href*="products_search"]');
+
+    imgComodoSecure: ElementFinder = $('.secure>img');
+
+
+
+    navigateTo() {
+        return browser.get('/');
+    }
+
+    getBasketText() {
+        return $('.basket a').getText();
+
+    }
+}
